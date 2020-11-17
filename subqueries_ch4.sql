5464a669d03d5d394cf5f0eda061dbc88fc7c8c4
alanlyyy <44736157+alanlyyy@users.noreply.github.com>
2020-11-17 11:53:41 -0800

    p.137, (165/442)
    Continued working on Ch 4. subqueries

--- subqueries_ch4.sql
+++ subqueries_ch4.sql
@@ -47,3 +47,77 @@ WHERE empid =
 WHERE E.lastname LIKE N'A%');
 
 --p.131, 159/442
+
+--Multivalued subquery
+
+-- using TSQL data base,
+-- I want to display orderid from Sales.Orders table
+-- I filter results for empid from Sales.Orders table matching
+-- empid from HR.Employees table if last name starts with D.
+USE TSQL2012;
+SELECT orderid
+FROM Sales.Orders
+WHERE empid IN 
+	(SELECT E.empid 
+	FROM HR.Employees AS E
+	WHERE E.lastname LIKE N'D%');
+
+-- I want to display custid, orderid, orderdate, empid
+-- So I query rows from Sales.Orders table
+-- I filter for custid from Sales.Orders table to match
+-- the set of custid from Sales.Customers table where country = USA
+SELECT custid, orderid, orderdate, empid
+FROM Sales.Orders
+WHERE custid IN 
+		(SELECT C.custid FROM Sales.Customers AS C
+		WHERE C.country = N'USA');
+
+-- I want to display custid, companyname from Sales.Customers table
+-- I filter for custid from Sales.Customers table
+-- not found in the set of custid found in the Sales.Orders table
+-- "in other words, return customers who did not place an order."
+SELECT custid, companyname
+FROM Sales.Customers
+WHERE custid NOT IN
+(SELECT O.custid
+FROM Sales.Orders AS O);
+
+--Using multiple subqueries in a query
+
+-- Create a new tabl called dbo.Orders
+-- where orderid is type int and primary key.
+IF OBJECT_ID('dbo.Orders', 'U') IS NOT NULL DROP TABLE dbo.Orders;
+CREATE TABLE dbo.Orders(orderid INT NOT NULL CONSTRAINT PK_Orders PRIMARY KEY);
+
+--insert data into orderid column
+--a copy of orderid from Sales.Orders table that are even
+INSERT INTO dbo.Orders(orderid)
+	SELECT orderid
+	FROM Sales.Orders
+	WHERE orderid % 2 = 0;
+
+-- I want to display n from dbo.Nums table
+-- I filter for n, if between min orderid from dbo.Orders table (scalar subquery)
+-- and max orderid from dbo.Orders table (scalar subquery)
+-- and if n is not an orderid found in dbo.Orders table. (multi- subquery)
+-- returns all odd numbered orderid between min and max order ids found in orders table.
+SELECT n
+FROM dbo.Nums
+WHERE n BETWEEN (SELECT MIN(O.orderid) FROM dbo.Orders AS O)
+				AND (SELECT MAX(O.orderid) FROM dbo.Orders AS O)
+AND n NOT IN (SELECT O.orderid FROM dbo.Orders AS O);
+
+
+--Correlated Subqueries
+
+USE TSQL2012;
+
+-- I want to display custid, orderid, orderdate, empid from Sales.Orders 1 table
+-- for each row I filter for orderid = to (MAX orderid for current custid) 
+SELECT custid, orderid, orderdate,empid
+FROM Sales.Orders AS O1
+WHERE orderid = (SELECT MAX(O2.orderid)
+				FROM Sales.Orders AS O2
+				WHERE O2.custid = O1.custid);
+
+--p.137 165/442
