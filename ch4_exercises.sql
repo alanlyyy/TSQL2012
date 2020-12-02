59c9f85c3b287824ee1706afaf93164c979a76b5
alanlyyy <44736157+alanlyyy@users.noreply.github.com>
2020-12-02 09:11:21 -0800

    p.152 - 1 hour
    Solved exercises 4-8 for 12-01-20 and 12-02-20
    Need to review solutions on 12-03-20

--- ch4_exercises.sql
+++ ch4_exercises.sql
@@ -50,3 +50,100 @@ IF OBJECT_ID('dbo.temp_table', 'U') IS NOT NULL
 DROP TABLE dbo.temp_table;
 
 --page 147, 175/442 exercise 3.
+
+USE TSQL2012;
+SELECT * FROM Sales.Orders;
+SELECT * FROM HR.Employees;
+
+-- I want to display empid, firstname, lastname
+-- so I query rows from HR.Employees table
+-- I filter each rows empid, if the empid is not found with an orderdate >=20080501 from Sales.Orders table.
+SELECT empid, firstname,lastname 
+FROM HR.Employees
+WHERE empid NOT IN (SELECT SO.empid
+				FROM Sales.Orders AS SO
+				WHERE SO.orderdate >= '20080501');
+
+--4.
+USE TSQL2012;
+SELECT * FROM Sales.Customers;
+SELECT * FROM HR.Employees;
+
+-- I want to display unique countries
+-- so i query all rows from Sales.Customers table
+-- I look at each row of Sales.Customers.country and filter for country not found in HR.Employees
+SELECT DISTINCT C.country
+FROM Sales.Customers AS C
+WHERE C.country NOT IN (SELECT H.country 
+						FROM HR.Employees AS H)
+
+--5.
+-- I want to display custid, orderid, orderdate, empid
+-- I query all rows from Sales.Orders table
+-- I filter each row of  O1.orderdate if O1.orderdate is (O2.orderdate, latest date = top(1), sorted by desc) for the current rows custid
+-- Sort by custid ASC.
+USE TSQL2012;
+SELECT O1.custid,O1.orderid, O1.orderdate,O1.empid
+FROM Sales.Orders AS O1
+WHERE O1.orderdate = (SELECT TOP(1) O2.orderdate
+					FROM Sales.Orders AS O2
+					WHERE O2.custid = O1.custid
+					ORDER BY O2.orderdate DESC)
+ORDER BY O1.custid;
+					
+--6.
+USE TSQL2012;
+SELECT * FROM Sales.Customers;
+SELECT * FROM Sales.Orders;
+
+-- I want to display custid, and companyname
+-- So i query all rows from Sales.Customers table
+-- I look at each row of custid, accept custid if contains an orderdate in 2007
+-- AND if custid does not contain an orderdate in 2008.
+-- order by ascending
+SELECT C.custid, C.companyname
+FROM Sales.Customers AS C
+WHERE				custid  IN (SELECT O.custid 
+							FROM Sales.Orders AS O
+							WHERE YEAR(O.orderdate) = '2007')
+
+			AND 
+					custid NOT IN(SELECT O2.custid
+								FROM Sales.Orders AS O2
+								WHERE YEAR(O2.orderdate) = '2008')
+				
+ORDER BY C.custid;
+
+--7.
+SELECT * FROM Sales.Customers;
+SELECT * FROM Sales.Orders;
+SELECT * FROM Sales.OrderDetails;
+
+
+-- I want to display custid, companyname
+-- So I query rows from Sales.Customers table, 
+-- I filter rows of custid for custid that contains and orderid that contains a productid of 12
+SELECT O1.custid, O1.companyname
+FROM Sales.Customers AS O1
+WHERE O1.custid IN (SELECT O2.custid 
+					FROM Sales.Orders AS O2
+					WHERE O2.orderid IN (SELECT O3.orderid
+										FROM Sales.OrderDetails AS O3
+										WHERE O3.productid = 12))
+
+--8.
+SELECT * FROM Sales.CustOrders
+ORDER BY custid ASC;
+
+-- I want to display custid, ordermonth, and qty
+-- runqty is the sum of the qty for each current rows custid (C2.custid and C.custid), up to the current rows order month (C2.ordermonth = C.ordermonth)
+-- We query rows from Sales.CustORders table.
+-- sort the table by custid ascending order.
+SELECT C.custid, C.ordermonth,C.qty,
+		(SELECT SUM(C2.qty)
+		FROM Sales.CustOrders AS C2
+		WHERE C2.ordermonth <= C.ordermonth AND C2.custid = C.custid) AS runqty
+FROM Sales.CustOrders AS C
+ORDER BY custid ASC;
+
+--solutions 152, 180/442
